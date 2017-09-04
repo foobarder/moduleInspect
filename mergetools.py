bc5a3731e7b81ea764d7d078e2ad02be2a0c36ae
igor <ishandrov@yahoo.co.uk>
2017-09-04 15:28:25 +0300

    added doc strings to the functions from the mergetools module

--- mergetools.py
+++ mergetools.py
@@ -1,8 +1,11 @@
 def join_members(*members):
+    """join_memebers(member1, ...) takes an orbitrary number of
+    arguments of type 'str'and concatinates them with using the '.' separator"""
     return '.'.join(members)
 
 
 def merge(dictionaryA, dictionaryB):
+    """merge(dict_A, dict_B) merges two dictionaries"""
     d = dictionaryA.copy()
     d.update(dictionaryB)
     return d
