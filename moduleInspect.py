d9f84c57631a441327fc85fc480e12476917c4e6
igor <ishandrov@yahoo.co.uk>
2017-09-08 12:15:11 +0300

    handles nans and generates unittest

--- moduleInspect.py
+++ moduleInspect.py
@@ -9,14 +9,20 @@ import inspect
 import future
 import pandas
 import pydoc
+import regex
 import past
+import math
 import sys
+import ast
 import re
 import os
 
 
+NAN = float('NaN')
+
+
 class Module:
-    """class Module(module_name) 
+    """class Module(module_name)
     Class Module gets all the submodules, classes, class_methods and functions of 
     the module taken as an argument by its instance. Every instance of the class has:
     ATTRIBUTES: Module.module_name, self.module, self.submodules, self.functions, self.classes, self.class_methods
@@ -109,41 +115,67 @@ def strip_text(string):
     return re.sub('\x08.', '', string.replace('\n', ''))
 
 
-
-def get_argnames(obj):
-    """get_argnames(obj) return a tuple of the object argument names."""
-    if inspect.isbuiltin(obj):
-        function_name = get_name(obj)
-        function_doc = strip_text(pydoc.render_doc(obj))
-        pattern = function_name + '\(.+?\)'
-        match = re.search(pattern, function_doc)
-        if match:
-            args = match.group().strip(function_name).translate(None, '()').split(',')
-            return tuple(re.search('\w+', arg).group() if re.search('\w+', arg) else None for arg in args)
+def get_signature(obj):
+    obj_name = obj.__name__
+    obj_doc = strip_text(pydoc.render_doc(obj))
+    match = regex.findall(obj_name + '(\((?>[^()]+|(?1))*\))', obj_doc)[:2]
+    if match:
+        if len(match) > 1:
+            signature = match[0][1:-1] if match[0][1:-1] != '...' and match[0] != '' else match[1][1:-1]
+            return signature
         else:
-            return (None,)
+            return match[0][1:-1]
     else:
-        return tuple(inspect.getargspec(obj).args)
+        return None
 
 
-def get_argtypes(obj, defaults):
-    """get_argtypes(obj, defaults) return a tuple of the object argument types."""
-    return ('arg_type',) * len(defaults)
-
-
-def get_argdefvalues(obj, args):
-    """get_argdefvalues(obj, args) return a tuple of the object argument default values."""
+def get_args(obj):
+    arguments = {}
     if inspect.isbuiltin(obj):
-        return ('builtin arg value',) * len(args)
+        obj_signature = get_signature(obj)
+        if obj_signature:
+            pattern = "\w+=[-+]?[0-9]*\.?[0-9]+|\w+=\w+|\w+=\[.+?\]|\w+=\(.+?\)|[\w=']+"
+            items = regex.findall(pattern, obj_signature)
+            for item in items:
+                split_item = item.split('=')
+                if len(split_item) == 2:
+                    arguments[split_item[0]] = split_item[1]
+                elif len(split_item) == 1:
+                    arguments[split_item[0]] = NAN
+            return arguments
+        else:
+            return {}
     else:
         argspec = inspect.getargspec(obj)
-        arguments = argspec.args
+        args = argspec.args
         defaults = argspec.defaults
         if defaults:
-            signature = dict(zip(arguments[-len(defaults):], defaults))
-            return tuple(signature[arg] if arg in signature else None for arg in args)
+            args_with_default_values = dict(zip(args[-len(defaults):], defaults))
+            for arg in args:
+                if arg in args_with_default_values:
+                    arguments[arg] = args_with_default_values[arg]
+                else:
+                    arguments[arg] = NAN
+            return arguments
         else:
-            return (None,) * len(args)
+            return dict(zip(args, [NAN] * len(args)))
+
+
+def guess_arg_type(arg_default_values):
+    types = []
+    for arg_value in arg_default_values:
+        if isinstance(arg_value, str):
+            try:
+                types.append(type(ast.literal_eval(arg_value)).__name__)
+            except ValueError:
+                types.append('str')
+            except SyntaxError:
+                types.append(NAN)
+        elif isinstance(arg_value, float) and math.isnan(arg_value):
+            types.append(NAN)
+        else:
+            types.append(type(arg_value).__name__)
+    return tuple(types)
 
 
 def get_arginfo(obj, args):
@@ -162,6 +194,27 @@ def merge(dictionaryA, dictionaryB):
     d = dictionaryA.copy()
     d.update(dictionaryB)
     return d
+
+
+def generate_unittest(function, args, prefix, filename='unittest.txt'):
+    function_signature = get_signature(function)
+    arg_count = 1
+    if function_signature:
+        with open(filename, 'a') as testfile:
+            for item in args.items():
+                try:
+                    float(item[1])
+                    if not math.isnan(item[1]):
+                        testfile.write("a{}={}\n".format(arg_count, item[1]))
+                    elif item[0] != 'self':
+                        testfile.write("a{}=\n".format(arg_count))
+                except (ValueError, TypeError):
+                    if item[0] != 'self':
+                        testfile.write("a{}={}\n".format(arg_count, item[1]))
+                    else:
+                        pass
+                arg_count += 1
+            testfile.write(prefix + '(' + function_signature + ')\n\n\n')
 
 
 def generate_documentation(module_name):
@@ -183,10 +236,12 @@ def generate_documentation(module_name):
         doc_frame['prefix'].append(get_prefix(member_name))
         doc_frame['function_name'].append(get_name(member))
         doc_frame['function_doc'].append(get_doc_string(member))
-        doc_frame['argument'].append(get_argnames(member))
-        doc_frame['argument_default_value'].append(get_argdefvalues(member, doc_frame['argument'][-1]))
-        doc_frame['argument_type'].append(get_argtypes(member, doc_frame['argument_default_value'][-1]))
+        doc_frame['argument'].append(tuple(get_args(member).keys()))
+        doc_frame['argument_default_value'].append(tuple(get_args(member).values()))
+        doc_frame['argument_type'].append(guess_arg_type(doc_frame['argument_default_value'][-1]))
         doc_frame['argument_info'].append(get_arginfo(member, doc_frame['argument'][-1]))
+
+        generate_unittest(member, get_args(member), doc_frame['prefix'][-1])
     return doc_frame
 
 
@@ -209,4 +264,4 @@ def main(module_name):
 
 
 if __name__ == "__main__":
-    main('json')
+    main('numpy')
